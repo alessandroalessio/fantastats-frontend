a00f7b2f7ddd284ee86638ecb017fb8dc40ea946
alessandroalessio <a.alessio86@gmail.com>
2022-07-19 19:07:58 +0200

    Changed something...

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar (){
                 <Link href="/">
                     <a className="normal-case">
                         <p className="block text-4xl font-bold tracking-tighter leading-7">{commonData.SiteName}</p>
-                        <span className="block text-2xs font-semibold italic text-accent leading-3">{commonData.Slogan}</span>
+                        <span className="block text-2xs font-semibold italic text-accent leading-3 text-center sm:text-left">{commonData.Slogan}</span>
                     </a>
                 </Link>
             </div>
@@ -32,7 +32,13 @@ function Navbar (){
                 {/* <button className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                 </button> */}
-                <a className="btn">Registrati</a>
+                <div className="hidden sm:inline-block">
+                    <a className="btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         </>
