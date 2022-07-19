c61ada0933b1f6f4a895d4feb830aa6180fb7f46
Alessandro <info@a2area.it>
2022-07-19 18:06:44 +0200

    Added demo content

--- pages/stats-giocatori.js
+++ pages/stats-giocatori.js
@@ -209,7 +209,7 @@ export default function StatsGiocatori(props) {
 
                     <DataTable
                         columns={columns}
-                        data={data}
+                        data={props.data}
                     />
 
                     <br />
