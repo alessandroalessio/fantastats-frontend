02f3f6addf5d338a22d36461e028bf408813e2e0
Alessandro Alessio <info@a2area.it>
2022-08-06 16:40:07 +0200

    Bugfixes on global player data

--- pages/giocatore/[id].js
+++ pages/giocatore/[id].js
@@ -218,7 +218,7 @@ function StatsGiocatoriSingle(props) {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
                         </div>
                         <div className="stat-title">Partite giocate</div>
-                        <div className="stat-value">{ props.playerData.mv }</div>
+                        <div className="stat-value">{ props.playerData.pg }</div>
                         {/* <div className="stat-desc text-secondary">31 tasks remaining</div> */}
                     </div>
                 </div>
