3a81e8d6a35d5ef8fad48d45523b51eeeaff5029
Alessandro Alessio <info@a2area.it>
2023-08-01 10:48:35 +0200

    Changed footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,23 @@
-const commonData = require('../data/common.json')
+import Link from 'next/link';
+const commonData = require('../data/common.json');
 
-function Footer(){
-    return(
-        <>
-            <div className="w-full text-center p-8 mt-4 text-sm">
-                <span className="block"><strong>{commonData.SiteName}</strong> - <i>{commonData.Slogan}</i></span>
-            </div>
-        </>
-    )
+function Footer() {
+	return (
+		<>
+			<div className="block w-full text-center p-8 text-sm border-t mt-20">
+				<span className="block">
+					<strong>{commonData.SiteName}</strong> -{' '}
+					<i>{commonData.Slogan}</i>
+				</span>
+				<small className="flex gap-1 justify-center mt-2">
+					<span>Fantastats è un progetto di </span>
+					<Link href="https://www.alessandroalessio.dev">
+						<a className="underline">Alessandro Alessio</a>
+					</Link>
+				</small>
+			</div>
+		</>
+	);
 }
 
-export default Footer
+export default Footer;
