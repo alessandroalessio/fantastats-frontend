84820d68912b08efc7de817f18115c45ce686290
Alessandro Alessio <info@a2area.it>
2024-06-09 17:12:41 +0200

    Try to add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 class MyDocument extends Document {
 	render() {
@@ -16,6 +17,7 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;0,700;0,800;1,200;1,400;1,600;1,700;1,800&display=swap"
 						rel="stylesheet"
 					/>
+					<GoogleAnalytics />
 				</Head>
 				<body>
 					<Navbar />
